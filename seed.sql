8fe0a8a2d1e5da580c797d3e5c7d4ebc2beaa723
javimarashall <jamondragon@ucdavis.edu>
2021-04-24 09:40:45 -0700

    names

---
+++ seed.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+    VALUES ("Javier", "Mondragon", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Andrew", "Luck", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Aaron", "Rodgers", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Patrick", "Mahomes", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Tom", "Brady", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Russel", "Wilson", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+    VALUES ("Javier", "Mondragon", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Drew", "Brees", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Lamar", "Jackson", , );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+    VALUES ("Matt", "Stafford", , );
+
