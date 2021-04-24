12a2ad7221cfe243485dc7eb6fc5e87d2d747546
javimarashall <jamondragon@ucdavis.edu>
2021-04-24 16:02:58 -0700

    tables fix

--- schema.sql
+++ schema.sql
@@ -1,22 +1,22 @@
---Drop an existing database
+-- Drop an existing database
 DROP DATABASE IF EXISTS employee_DB;
---Create new database
+-- Create new database
 CREATE DATABASE employee_DB;
---Use this database
+-- Use this database
 USE employee_DB;
 
---create department table
-CREATE TABLE department (
+-- create department table
+CREATE TABLE department(
     id INT(10) AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NOT NULL, --Hold department name
+    name VARCHAR(30) NOT NULL, -- Hold department name
     PRIMARY KEY(id)
 );
 
 CREATE TABLE role(
-    id INT(10) AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30), --hold role title
-    salary DECIMAL(10,2), --hold role salary
-    department_id INT(10), --to hold reference to department role belongs to
+    id INT(10) AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL, -- hold role title
+    salary DECIMAL(10,2) NOT NULL, -- hold role salary
+    department_id INT(10) NOT NULL, -- to hold reference to department role belongs to
     PRIMARY KEY(id)
 );
 
@@ -25,6 +25,6 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10) NOT NULL,
-    manager_id INT(10) NOT NULL, -- to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-    PRIMARY KEY(id) NULL
+    manager_id INT(10), -- to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    PRIMARY KEY(id)
 );
--- seed.sql
+++ seed.sql
@@ -1,40 +1,48 @@
 
 -- Insert employees to employee table
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-    VALUES ("Javier", "Mondragon", , );
+    VALUES ("Javier", "Mondragon", 1, 2);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Andrew", "Luck", , );
+    VALUES ("Andrew", "Luck", 2, null);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Aaron", "Rodgers", , );
+    VALUES ("Aaron", "Rodgers", 3, 4);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Patrick", "Mahomes", , );
+    VALUES ("Patrick", "Mahomes", 4, null);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Tom", "Brady", , );
+    VALUES ("Tom", "Brady", 5, 6);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Russel", "Wilson", , );
+    VALUES ("Russel", "Wilson", 6, null);
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-    VALUES ("Javier", "Mondragon", , );
+    VALUES ("Dereck", "Carr", 7, null);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Drew", "Brees", , );
+    VALUES ("Drew", "Brees", 1, 2);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Lamar", "Jackson", , );
+    VALUES ("Lamar", "Jackson", 3, 4);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-    VALUES ("Matt", "Stafford", , );
+    VALUES ("Matt", "Stafford", 5, 6);
 
---Insert titles into role table
+-- Insert titles into role table
+INSERT INTO employee_role(title, salary, department_id)
+    VALUES("Sales", 60000, 1);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Sales", 60000, 1)
+    VALUES("Sales Lead", 80000, 1);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Sales Lead", 80000, 1)
+    VALUES("Engineer", 90000, 2);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Engineer", 90000, 2)
+    VALUES("Engineer Lead", 100000, 2);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Engineer Lead", 100000, 2)
+    VALUES("Accountant", 70000, 3);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Accountant", 70000, 3)
+    VALUES("Accounting Lead", 80000, 3);
 INSERT INTO role(title, salary, department_id)
-    VALUES("Accounting Lead", 80000, 3)
-INSERT INTO role(title, salary, department_id)
-    VALUES("Lawyer", 90000, 4)
+    VALUES("Lawyer", 90000, 4);
 
---Insert departments into department table
+-- Insert departments into department table
+INSERT INTO department(name)
+    VALUES("sales");
+INSERT INTO department(name)
+    VALUES("Engineering");
+INSERT INTO department(name)
+    VALUES("Accounting");
+INSERT INTO department(name)
+    VALUES("Legal");
